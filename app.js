f83f5a37d4a1b5dc2c1a193e443ec43d4da569af
ABHISHEK KUMAR <amreshk005@gmail.com>
2020-08-17 22:58:44 +0530

    cors updated

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV == "dev") {
 }
 
 app.all("/*", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://ekart005.web.app");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS"));
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
