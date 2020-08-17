e6de43416b16a16d9fad8cbfdec2646c6af31713
ABHISHEK KUMAR <amreshk005@gmail.com>
2020-08-17 14:25:40 +0530

    changing the deploy of backend

--- Backend/models/cartModel.js
+++ Backend/models/cartModel.js
@@ -1,26 +1,35 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const User = require("./userModel");
-const Product = require("./productsModel");
 
-let cartSchema = new Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  products: [
-    {
-      productId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-      },
-      quantity: Number,
-      name: String,
-      price: Number,
+let cartSchema = new Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
-  ],
-  createdOn: {
-    type: Date,
-    default: Date.now,
+    products: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        quantity: Number,
+        name: String,
+        price: Number,
+      },
+    ],
+    createdOn: {
+      type: Date,
+      default: Date.now,
+    },
+    subTotal: {
+      default: 0,
+      type: Number,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("cart", cartSchema);
